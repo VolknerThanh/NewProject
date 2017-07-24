18d962fb5561e7cafe895b99911d880e5adacfef
Silver Soul <nhankimthanh1997@gmail.com>
2017-07-25 00:43:35 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("Scroll-to-top-button").style.display = "block";
+    } else {
+        document.getElementById("Scroll-to-top-button").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
